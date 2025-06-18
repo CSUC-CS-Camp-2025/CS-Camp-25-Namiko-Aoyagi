5cae0c1857f1d896149c0879707abab88526b220
churchofidiots <wolfnamiko@gmail.com>
2025-06-18 17:04:54 +0000

    new code

--- js/main.js
+++
@@ -1,5 +0,0 @@
-$( document ).ready(function() {
-    $('#btn').click(function() {
-        window.alert("Congratulations, you found nothing! Check your email for a prize ariving never!");
-    });
-});
